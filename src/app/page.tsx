a621eff6b322a35c50b5c4cdbc82395548fa0cda
Jaron Lin <112691692+jaroonl@users.noreply.github.com>
2025-08-01 17:45:50 -0700

    Revert "Initialized homepage for website"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,10 @@
-import Header from "../components/Header";
-import Hero from "../components/Hero";
-import Footer from "../components/Footer";
-import WeeklySchedule from "../components/WeeklySchedule"
-import UpcomingEvents from "../components/UpcomingEvents";
 import AboutMePage from "./aboutMe";
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
-      <Header />
-            <AboutMePage />
-      <Hero />
-      <section className="py-16 bg-white">
-        <WeeklySchedule />
-      </section>
-      <section className="py-16 bg-white">
-        <UpcomingEvents />
-      </section>
-      <Footer />
-    </div>
-
+    <main>
+      <AboutMePage />
+      
+    </main>
   );
 }
